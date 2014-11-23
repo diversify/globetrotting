82a84e2b44e9aafe235920a7a5c6790d5fcf8d4a
Dennis Jin <desuvader@gmail.com>
2014-11-23 09:37:57 +0100

    Add animations and st0ff

--- components.js
+++ components.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var map = null;
+var map;
+var overlay;
+var loadingTrack = false;
+
 var GtMap = React.createClass({
 	getInitialState: function() {
 		return {
@@ -57,14 +60,62 @@ var GtMap = React.createClass({
 				lng: markerInfo.lng,
 			},
 			map: map,
+			icon: 'images/pin.gif',
+			origin: new google.maps.Point(0, 20),
+			size: new google.maps.Size(20, 20),
 			cityName: markerInfo.cityName,
 			countryName: markerInfo.countryName,
 			spotifyTrackIds: markerInfo.spotifyTrackIds
 		});
 
-		google.maps.event.addListener(marker, 'click', function() {
+		google.maps.event.addListener(marker, 'click', function(e) {
+			if (!this.state.visible) {
+				this.showPulse(overlay.getProjection().fromLatLngToContainerPixel(e.latLng));
+			} else {
+				this.showRing(overlay.getProjection().fromLatLngToContainerPixel(e.latLng));
+			}
+
 			this.playTrack(marker, Math.round(Math.random() * 10)); // Start with first in playlist
 		}.bind(this));
+	},
+	showPulse: function(projection) {
+		var $pulse = $('#pulse');
+		$pulse.css({
+			display: 'block',
+			top: projection.y - $pulse.height() / 2,
+			left: projection.x - $pulse.width() / 2
+		}).velocity({
+			scale: [30, 0.3],
+			opacity: [0, 0.8]
+		}, {
+			easing: [0.165, 0.84, 0.44, 1],
+			duration: 2000,
+			begin: function() {
+				setTimeout(function() {
+					$pulse.hide();
+				}, 2000);
+			}
+		});
+	},
+	showRing: function(projection) {
+		var $ring = $('#ring');
+		$ring.css({
+			display: 'block',
+			transform: 'scale(0)',
+			top: projection.y - $ring.height() / 2,
+			left: projection.x - $ring.width() / 2
+		}).stop(true).velocity({
+			scale: [1, 0],
+			opacity: [1, 0]
+		},{
+			easing: [0.1, 0.885, 0.07, 1.1],
+			duration: 1000
+		}).velocity({
+			scale: [0.8, 1]
+		}, {
+			loop: true,
+			easing: [0.215, 0.61, 0.355, 1]
+		});
 	},
 	playTrack: function(marker, trackIndex) {
 		this.getTrackInfo(marker.spotifyTrackIds[trackIndex], function(trackInfo) {
@@ -83,7 +134,7 @@ var GtMap = React.createClass({
 			this.setState({
 				cityName: marker.cityName,
 				countryName: marker.countryName,
-				trackName: trackInfo.name + ' by ' + trackInfo.artists[0].name,
+				trackName: trackInfo.name + ' - ' + trackInfo.artists[0].name,
 				previewUrl: trackInfo.preview_url,
 				progressPercent: 0,
 				currentMarker: marker,
@@ -102,6 +153,17 @@ var GtMap = React.createClass({
 			}.bind(this));
 		}.bind(this));
 	},
+	playPauseTrack: function() {
+		var fieldNameElement = document.getElementById('play-pause-btn');
+
+		if (this.state.currentTrack.isPaused()) {
+			this.state.currentTrack.play();
+			fieldNameElement.innerHTML = 'Play';
+		} else {
+			this.state.currentTrack.pause();
+			fieldNameElement.innerHTML = 'Pause';
+		}
+	},
 	drawPolyLine: function(newMarker) {
 		if (this.state.currentMarker && this.state.currentMarker.cityName !== newMarker.cityName) {
 
@@ -110,12 +172,10 @@ var GtMap = React.createClass({
 				new google.maps.LatLng(newMarker.position.k, newMarker.position.B)
 			];
 
-			console.log(linePathLatLng);
-
 			new google.maps.Polyline({
 				path: linePathLatLng,
 				geodesic: true,
-				strokeColor: '#81b900',
+				strokeColor: '#ffffff',
 				strokeOpacity: 1,
 				strokeWeight: 2,
 				map: map
@@ -132,9 +192,12 @@ var GtMap = React.createClass({
 		});
 	},
 	getTrackInfo: function(spotifyTrackId, callback){
+		if (loadingTrack) return false;
+		loadingTrack = true;
 		$.ajax({
 			url: 'https://api.spotify.com/v1/tracks/' + spotifyTrackId,
 			success: function(res) {
+				loadingTrack = false;
 				callback(res);
 			}
 		});
@@ -254,16 +317,21 @@ var GtMap = React.createClass({
 		}];
 
 		var mapOptions = {
-			center: new google.maps.LatLng(20, 50),
+			center: new google.maps.LatLng(20, 0),
 			zoom: 3,
-			maxZoom: 6,
+			maxZoom: 10,
 			minZoom: 3,
 			scrollwheel: false,
 			//navigationControl: false,
 			//mapTypeControl: false,
-			//scaleControl: false,
+			scaleControl: true,
+			zoomControl: true,
+			zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.LARGE,
+				position: google.maps.ControlPosition.RIGHT_CENTER
+			},
 			//draggable: false,
-			//panControl: false,
+			panControl: false,
 			//zoomControl: false,
 			streetViewControl: false,
 			//overviewMapControl: false,
@@ -273,6 +341,10 @@ var GtMap = React.createClass({
 		}
 
 		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+		overlay = new google.maps.OverlayView();
+		overlay.draw = function() {};
+		overlay.setMap(map);
 	},
 	render: function() {
 		var progressStyle = {
@@ -290,7 +362,10 @@ var GtMap = React.createClass({
 						</div>
 					</div>
 				<div onClick={this.skipTrack}>
-					SKIP IT
+					Skip song
+				</div>
+				<div id="play-pause-btn" onClick={this.playPauseTrack}>
+					Play
 				</div>
 				</div>
 				<div id="progress-wrap" onClick={this.setProgress}>
